706c6133a9478bb4ea9354f81e490dc1beea52df
JustRacc00n <barteklew123@gmail.com>
2022-06-25 15:27:10 +0200

    Usunięcie zbędnych funkcji zamknięcia pliku z logami. Finalne testy działania programu zapisane w pliku "log.txt".

--- PSIZ4/PSIZ4.cpp
+++ PSIZ4/PSIZ4.cpp
@@ -232,7 +232,6 @@ int main(int argc, char* argv[])
                 zapiszL("Obliczanie danych dla plikow.");
                 wynik = oblicz(sciezka1, sciezka2);
                 drukowanieWynikow(wynik);
-                zamknijL();
             }
             break;
 
@@ -244,7 +243,6 @@ int main(int argc, char* argv[])
                 zapiszL("Obliczanie danych dla plikow.");
                 wynik = oblicz("t1_plik1.bin", "t1_plik2.bin");
                 drukowanieWynikow(wynik);
-                zamknijL();
             }
             break;
 
@@ -256,7 +254,6 @@ int main(int argc, char* argv[])
                 zapiszL("Obliczanie danych dla plikow.");
                 wynik = oblicz("t2_plik3.bin", "t2_plik4.bin");
                 drukowanieWynikow(wynik);
-                zamknijL();
             }
             break;
 
@@ -268,7 +265,6 @@ int main(int argc, char* argv[])
                 zapiszL("Obliczanie danych dla plikow.");
                 wynik = oblicz("t3_plik5.bin", "t3_plik6.bin");
                 drukowanieWynikow(wynik);
-                zamknijL();
             }
             break;
 
